7a996dcd1130eaa3c67fbb6a5a753325b5ca74cb
Sam Bostock <bostocksamuel@gmail.com>
2017-04-09 18:40:49 -0400

    Initial commit

---
+++ lib/fizz_badd.ex
@@ -0,0 +1,18 @@
+defmodule FizzBadd do
+  @moduledoc """
+  Documentation for FizzBadd.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FizzBadd.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fizz_badd_test.exs
@@ -0,0 +1,8 @@
+defmodule FizzBaddTest do
+  use ExUnit.Case
+  doctest FizzBadd
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
